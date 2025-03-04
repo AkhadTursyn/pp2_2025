dae7871fdd6689d79e1257d4d009721980e57a45
Ahad <ahadtursyn07@gmail.com>
2025-03-04 12:03:05 +0500

    update

---
+++ lab5/4.py
@@ -0,0 +1,9 @@
+import re
+
+pattern = r"^[A-Z][a-z]+"
+test_strings = ["Hello", "hello", "Test", "TEST", "Python"]
+
+for s in test_strings:
+    if re.fullmatch(pattern, s):
+        print(f"Matched: {s}")
+        
